ba3a8aa3a4c9eafd77127a7858bae3d85183baaf
essattv <158786347+essattv@users.noreply.github.com>
2024-11-02 21:02:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 from datetime import datetime, timedelta
 from system import SystemRaportowania
+from functools import wraps
 import tempfile
 import pdfkit
 
